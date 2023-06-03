558376bbedbd9c5ae89fbbbfe78261b6eff88af5
Romanivanov111 <121168386+Romanivanov111@users.noreply.github.com>
2023-06-03 11:28:31 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        String name = "Roman";
+        String surname = "Ivanov";
+        int income = 99999;
+        int spending = 55555;
+        int surplus = income - spending;
+        System.out.println("Roman Ivanov");
+        System.out.println(name + " " + surname + " " + surplus);
+    }
 }
